06e8b2e6d43c8dfb3307c075553268b8cbeeb543
dominhloc <dominhloc2510@gmail.com>
2024-05-09 15:57:45 +0700

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,11 @@ let id = 0
 const newTodo = ref('')
 const hideCompleted = ref(false)
 const todos = ref([
-    { id: id++, text: 'Nấu Cơm', done: false },
-    { id: id++, text: 'Rửa Bát', done: false },
-    { id: id++, text: 'Quét Nhà', done: false },
-    { id: id++, text: 'Viec 4', done: false },
-    { id: id++, text: 'Viec 5', done: false },
+    { id: id++, text: 'Công Việc 1', done: false },
+    { id: id++, text: 'Công Việc 2', done: false },
+    { id: id++, text: 'Công Việc 3', done: false },
+    { id: id++, text: 'Công Việc 4', done: false },
+    { id: id++, text: 'Công Việc 5', done: false },
 ])
 
 const filteredTodos = computed(() => {
@@ -49,8 +49,8 @@ function getTodoClass(todo) {
 <template>
     <div class="bg-blue-200 h-screen flex flex-col justify-center items-center">
 
-        <div class="container mx-auto p-4 bg-white">
-            <h1 class=" text-3xl px-10 text-left font-bold ">Todo App</h1><br>
+        <div class="container mx-auto p-4 bg-white"><br>
+            <h1 class=" text-3xl px-10 text-left font-bold ">Todo App</h1>
             <form class="flex flex-row px-10 gap-2 " @submit.prevent="addTodo">
                 <input class=" bg-slate-300 text-right px-6 w-full font-bold" v-model="newTodo" required
                     placeholder="Add your new todo">
@@ -68,8 +68,9 @@ function getTodoClass(todo) {
                                 :class="getTodoClass(todo)">{{
                                     todo.text }}
                             </span>
-                            <button class="text-white bg-blue-500 rounded mr-5 flex-grow-0 w-16"
-                                @click="removeTodo(todo)">Delete</button>
+                            <button class="text-white rounded mr-5 flex-grow-0 w-16" @click="removeTodo(todo)"><img
+                                    src="https://banner2.cleanpng.com/20180204/tew/kisspng-button-icon-delete-button-png-picture-5a77bb72658409.7623834215177962104158.jpg"
+                                    class="w-5 h-5 rounded"></button>
                         </div>
                     </div>
                 </li>
---
+++ pages/test.vue
@@ -0,0 +1,9 @@
+<script>
+import { ref } from "vue";
+const newTodo = ref('')
+</script>
+
+
+<template>
+    <h1>Tic Tac Toe</h1>
+</template>
