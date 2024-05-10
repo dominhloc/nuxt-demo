bc44e71f4fe30b60aea5db9f2f981ab640feaae9
dominhloc <dominhloc2510@gmail.com>
2024-05-10 14:25:26 +0700

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,15 @@
 <script setup>
 import { ref, computed } from "vue";
 
-let id = 1;
 const newTodo = ref("");
 const hideCompleted = ref(false);
+
 const todos = ref([
-    { id: id++, text: "Công Việc 1", done: false },
-    { id: id++, text: "Công Việc 2", done: false },
-    { id: id++, text: "Công Việc 3", done: false },
-    { id: id++, text: "Công Việc 4", done: false },
-    { id: id++, text: "Công Việc 5", done: false },
-    { id: id++, text: 'Công Việc 6', done: false },
-    { id: id++, text: 'Công Việc 7', done: false },
-    { id: id++, text: 'Công Việc 8', done: false },
-    { id: id++, text: 'Công Việc 9', done: false }
+    { id: 1, text: "Công Việc 1", done: false },
+    { id: 2, text: "Công Việc 2", done: false },
+    { id: 3, text: "Công Việc 3", done: false },
+    { id: 4, text: "Công Việc 4", done: false },
+    { id: 5, text: "Công Việc 5", done: false },
 ]);
 
 const filteredTodos = computed(() => {
@@ -21,37 +17,39 @@ const filteredTodos = computed(() => {
 });
 
 function addTodo() {
+    let newIndex = todos.value.length + 1
     todos.value.push({
-        id: id++,
+        id: newIndex,
         text: newTodo.value,
         done: false,
     });
-    console.log("🚀 ~ todos.value:", todos.value);
+    // console.log(todos.value)
+    // xóa input đầu vào
     newTodo.value = "";
 }
 
 function removeTodo(todo) {
-    console.log("🚀 ~ todo:", todo);
-    todos.value = todos.value.filter((t) => t !== todo);
+    let x = todos.value.filter((t) => t.id !== todo.id);
+    todos.value = x
 }
 
 function getTodoClass(todo) {
     if (todo.done) {
-        return "line-through";
+        return "line-through text-red-500";
     } else {
-        return "text-red-50";
+        return "text-black-500";
     }
 }
 </script>
 
 <template>
-    <div class="bg-blue-400 h-screen flex flex-col justify-center items-center">
+    <div class="bg-cyan-900 h-screen flex flex-col justify-center items-center">
         <div class="container mx-auto p-4 flex flex-col bg-white h-[600px] w-[450px] 
-        space-y-3 rounded-xl shadow-md justify-between">
-            <h1 class=" text-3xl text-center font-bold ">Todo App</h1>
+        space-y-3 rounded-xl justify-between">
+            <h1 class=" text-3xl text-center font-bold  hover:bg-gray-300 duration-500 ">Todo App</h1>
             <form class="flex flex-row shadow" @submit.prevent="addTodo">
-                <input class="text-left px-6 w-full border-2 font-bold rounded-xl h-14" v-model="newTodo" required
-                    placeholder="Add a new task........ " />
+                <input class="text-left shadow-md px-6 w-full border-2 font-bold rounded-xl h-14" v-model="newTodo"
+                    required placeholder="Add a new task........ " />
             </form>
             <div class="space-y-5 h-full overflow-auto">
                 <div class="flex flex-col">
@@ -59,11 +57,10 @@ function getTodoClass(todo) {
                         <div class="flex flex-col h-auto w-full px-2">
                             <div class="flex items-center justify-between">
                                 <input type="checkbox" class="mr-2 flex-grow-0" v-model="todo.done" />
-                                <span class="!text-black font-bold text-start flex-grow word-break-all"
+                                <span class="font-bold text-start flex-grow word-break-all"
                                     :class="getTodoClass(todo)">{{ todo.text }}
                                 </span>
-                                <button class="rounded flex-grow-0 w-5 bg-red-500 text-white"
-                                    @click="removeTodo(todo)"><img
+                                <button class="flex-grow-0 w-6" @click="removeTodo(todo)"><img
                                         src="https://banner2.cleanpng.com/20180204/tew/kisspng-button-icon-delete-button-png-picture-5a77bb72658409.7623834215177962104158.jpg"
                                         class="w-5 h-5 rounded"></button>
                             </div>
@@ -72,7 +69,7 @@ function getTodoClass(todo) {
                 </div>
             </div>
             <div class="text-end mt-4 space-x-2">
-                <button class="bg-green-500 hover:bg-green-600 duration-500 text-white p-1 rounded-xl w-20"
+                <button class=" bg-green-500 hover:bg-green-600 duration-500 text-white p-1 rounded-xl w-20"
                     @click="hideCompleted = false">Show All</button>
                 <button class="bg-blue-500 hover:bg-blue-600 duration-500 text-white p-1 rounded-xl w-20"
                     @click="hideCompleted = true">Hide</button>
---
+++ pages/test.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+let id = 0
+let playEr1 = 'X'
+let playEr2 = 'O'
+const board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+];
+
+</script>
+
+
+<template>
+
+    <div class="flex flex-col justify-center items-center bg-cyan-950 h-screen">
+
+        <div class="h-[100px] w-[300px] text-white text-5xl font-bold flex flex-col">
+            <div class=" justify-between text-center">TIC TAC TOE</div>
+        </div>
+
+        <div class="border-white bg-slate-500 border-2 rounded-xl p-5 shadow">
+            <div class="text-center text-white mb-15 text-2xl">Lượt Người Chơi 'X'
+                <div class="m-5 flex flex-col justify-center items-center h-xs w-xs text-center bg-black rounded-xl">
+                </div>
+                <div class="mx-auto p-4 flex flex-col bg-white h-[300px] w-[300px] rounded-xl"></div>
+
+                <button class=" p-2 bg-cyan-500 shadow-lg shadow-cyan-50/50 hover:bg-cyan-700 rounded-xl "
+                    @click="hideCompleted = false">Bat Dau Lai</button>
+            </div>
+        </div>
+    </div>
+</template>
